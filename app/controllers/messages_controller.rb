9954c2354161219d7fa77911b6bb41fb491ede19
toexam <tomoyo.omae@gmail.com>
2019-05-02 13:49:58 +0900

    問題4メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,28 @@
-class MessageComtroller < ApplicationController
+class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @messages = @group.messages.incrudes(:user)
   end
 
   def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to_group_messages_path(@group),notice:'メッセージが送信されました'
+    else
+      @messages = @group.messages(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
   end
 end
+
+private
+
+  def message_params
+   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
+
