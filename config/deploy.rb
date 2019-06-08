78ba3da25268fe353534e4473c99d32d747ce54c
toexam <tomoyo.omae@gmail.com>
2019-06-08 18:12:27 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
+
 set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/TOAWS.pem']
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
